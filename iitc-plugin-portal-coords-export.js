79fb7be2f29f57d436fb75eb8700d9658108df8d
3nvy <renatompestana@gmail.com>
2020-04-28 21:51:17 +0100

    Add support to map a route manually

--- iitc-plugin-portal-coords-export.js
+++ iitc-plugin-portal-coords-export.js
@@ -16,6 +16,105 @@ function wrapper(plugin_info) {
 
   window.plugin.exportcoords = function () {};
 
+  window.plugin.exportcoords.setupCallback = function () {
+    if (!localStorage["selectiveCoordsList"])
+      localStorage["selectiveCoordsList"] = "[]";
+
+    addHook("portalDetailsUpdated", window.plugin.exportcoords.addLink);
+    window.plugin.exportcoords.refreshSelectedCoordinatesList();
+
+    window.plugin.exportcoords.drawSelectedCoordsPolyline();
+  };
+
+  /*********** Selected Coordinates ************************************************************/
+  window.plugin.exportcoords.addLink = function (d) {
+    $(".linkdetails").append(
+      `<aside><a onclick=\"window.plugin.exportcoords.addToList('${window.selectedPortal}')" title="Display raw data of the portal">Add for Selective Export</a></aside>`
+    );
+  };
+
+  window.plugin.exportcoords.addToList = function (guid) {
+    if (!window.portals[guid]) {
+      console.warn`Error: failed to find portal details for guid ${guid} - failed to show debug data`;
+      return;
+    }
+
+    const selectiveCoordsList = JSON.parse(localStorage["selectiveCoordsList"]);
+    var { latE6, lngE6, image, title } = window.portals[guid].options.data;
+
+    console.log(window.portals[guid].options.data);
+
+    if (selectiveCoordsList.find((p) => p.guid === guid)) return;
+    selectiveCoordsList.push({
+      guid,
+      lat: latE6 / 1000000,
+      lng: lngE6 / 1000000,
+      image,
+      title,
+    });
+
+    localStorage["selectiveCoordsList"] = JSON.stringify(selectiveCoordsList);
+    window.plugin.exportcoords.refreshSelectedCoordinatesList();
+  };
+
+  window.plugin.exportcoords.removeFromList = function (guid) {
+    console.log(guid);
+    var selectiveCoordsList = JSON.parse(localStorage["selectiveCoordsList"]);
+    var portals = selectiveCoordsList.filter((p) => p.guid !== guid);
+
+    localStorage["selectiveCoordsList"] = JSON.stringify(portals);
+    window.plugin.exportcoords.refreshSelectedCoordinatesList();
+  };
+
+  window.plugin.exportcoords.clearSelectedCoordinates = function () {
+    localStorage["selectiveCoordsList"] = "[]";
+    window.plugin.exportcoords.refreshSelectedCoordinatesList();
+  };
+
+  window.plugin.exportcoords.drawSelectedCoordsPolyline = function () {
+    if (window.plugin.exportcoords.selectedCoordsPolyline)
+      window.plugin.exportcoords.selectedCoordsPolyline.remove();
+
+    var selectiveCoordsList = JSON.parse(localStorage["selectiveCoordsList"]);
+    var pointList = selectiveCoordsList.map(
+      ({ lat, lng }) => new L.LatLng(lat, lng)
+    );
+
+    window.plugin.exportcoords.selectedCoordsPolyline = new L.Polyline(
+      pointList,
+      {
+        color: "red",
+        weight: 3,
+        opacity: 0.5,
+        smoothFactor: 1,
+      }
+    );
+    window.plugin.exportcoords.selectedCoordsPolyline.addTo(map);
+  };
+
+  window.plugin.exportcoords.refreshSelectedCoordinatesList = function () {
+    var selectiveCoordsList = JSON.parse(localStorage["selectiveCoordsList"]);
+    const firstEntries = selectiveCoordsList;
+
+    var html = firstEntries.map(
+      ({ image, title, guid }) => `
+    <div class="selected-coords-content">
+      <div class="selected-coords-content-info">
+        <img src="${image.replace("http:", "")}">
+        <span>${title}</span>
+      </div>
+      <div class="selected-coords-content-controls">
+        <a onclick="window.plugin.exportcoords.removeFromList('${guid}')" title="Remove Portal">X</a>
+      </div>
+    </div>
+    `
+    );
+
+    $("#selected-coordinates-mini").html(html);
+
+    window.plugin.exportcoords.drawSelectedCoordsPolyline();
+  };
+
   /*********** Menu ************************************************************/
   window.plugin.exportcoords.createmenu = function () {
     var shape = window.plugin.exportcoords.getFirstShape();
@@ -25,6 +124,11 @@ function wrapper(plugin_info) {
 
     <div class="flexed-box">
     <a onclick=\"window.plugin.exportcoords.export('CSV','VIEW');\" title='Export Portal Coordinates From ViewPort'>Export Current View</a>
+    ${
+      JSON.parse(localStorage["selectiveCoordsList"]).length !== 0
+        ? `<a onclick=\"window.plugin.exportcoords.export('CSV','SELECTED');\" title='Export Selected Portal Coordinates'>Export Selected</a>`
+        : ""
+    }
     ${
       plugin.drawTools && shape && shape.type === "polygon"
         ? `<a onclick=\"window.plugin.exportcoords.export('CSV','VIEWFIL');\" title='Export Portal Coordinates From Polygon'>Export Polygon</a>`
@@ -115,6 +219,7 @@ function wrapper(plugin_info) {
     } = window.plugin.exportcoords;
     var shape = window.plugin.exportcoords.getFirstShape();
 
+    var checkOnSelected = source === "SELECTED";
     var checkOnPolygon = source === "VIEWFIL" && shape.type === "polygon";
     var checkOnCircle = source === "VIEWFIL" && shape.type === "circle";
 
@@ -125,17 +230,24 @@ function wrapper(plugin_info) {
       checkOnPolygon ? "POLYGON" : checkOnCircle ? "CIRCLE" : "VIEWPORT"
     );
 
-    var allowedPortals = portals
-      .filter((p) => {
-        var latlng = `${p._latlng.lat},${p._latlng.lng}`;
+    var allowedPortals;
 
-        return checkOnPolygon
-          ? portalinpolygon(latlng, shape.latLngs)
-          : checkOnCircle
-          ? portalincircle(latlng, shape)
-          : portalinviewport(p._latlng.lat, p._latlng.lng, bounds);
-      })
-      .map((p) => `${p._latlng.lat},${p._latlng.lng}`);
+    if (checkOnSelected) {
+      allowedPortals = JSON.parse(localStorage["selectiveCoordsList"]).map(
+        ({ lat, lng }) => `${lat},${lng}`
+      );
+    } else
+      allowedPortals = portals
+        .filter((p) => {
+          var latlng = `${p._latlng.lat},${p._latlng.lng}`;
+
+          return checkOnPolygon
+            ? portalinpolygon(latlng, shape.latLngs)
+            : checkOnCircle
+            ? portalincircle(latlng, shape)
+            : portalinviewport(p._latlng.lat, p._latlng.lng, bounds);
+        })
+        .map((p) => `${p._latlng.lat},${p._latlng.lng}`);
 
     var dialog = window
       .dialog({
@@ -167,6 +279,17 @@ function wrapper(plugin_info) {
 
     $("head").append(`
       <style>
+          #sidebar {
+            display: -webkit-box;display: -ms-flexbox;display: flex;
+            -webkit-box-orient: vertical;-webkit-box-direction: normal;-ms-flex-direction: column;flex-direction: column;
+          }
+          #portaldetails {
+            min-height: unset !important;
+          }
+          img.fullimg {
+            display: none;
+          }
+
           .exportcoordsSetbox > a { display:block; color:#ffce00; border:1px solid #ffce00; padding:3px 0; margin:10px auto; width:100%; text-align:center; background:rgba(8,48,78,.9); }
           .ui-dialog-createmenu { width: 400px !important }
           .ui-dialog-export { width: 600px !important; max-width: 100% }
@@ -186,8 +309,42 @@ function wrapper(plugin_info) {
             -ms-flex-align: center;
             align-items: center;
           }
-          .flexed-box > a:first-child { margin-bottom: 10px }
+          .flexed-box > a { margin-bottom: 10px }
+
+          .selected-coords-content-info,
+          .selected-coords-content { 
+            display: -webkit-box;
+            display: -ms-flexbox;
+            display: flex;
+            -webkit-box-align: center;
+            -ms-flex-align: center;
+            align-items: center;
+            -webkit-box-ordinal-group: 2;-ms-flex-order: 1;order: 1
+           }
+          .selected-coords-content img { width: 35px; height: 35px; margin-right: 10px }
+
+          .selected-coords-content-controls {
+            -webkit-box-ordinal-group: 1;-ms-flex-order: 0;order: 0
+          }
+
+          .selected-coords-content-controls a{
+            margin: 0 10px
+          }
+
+          #selectedCoordsBox {
+            position: relative;
+            display: block !important;
+            overflow-y: scroll;
+          }
+
+          #selected-coordinates-mini {
+            margin-bottom: 7px;
+          }
+
           @media only screen and (max-width: 600px) {
+            #selectedCoordsBox {
+              overflow-y: unset;
+            }
             .ui-dialog-export {
               top: 0 !important;
               height: 100% !important;
@@ -206,6 +363,18 @@ function wrapper(plugin_info) {
           }
       </style>
     `);
+
+    var selectedCoordsBox = `
+    <div id="selectedCoordsBox" style="position: relative;">
+        <p style="margin: 5px 0 5px 0; text-align: center; font-weight: bold;">Selected Coordinates</p>
+        <a id="startScraper" style="position: absolute; top: 5px; left: 0; margin: 0 5px 0 5px;" onclick="window.plugin.exportcoords.clearSelectedCoordinates()" title="Clear Selected Coordinates">Clear</a>
+        <div id="selected-coordinates-mini"></div>
+    </div>
+    `;
+
+    $(selectedCoordsBox).insertBefore("#toolbox");
+
+    window.plugin.exportcoords.setupCallback();
   };
 
   setup.info = plugin_info; //add the script info data to the function as a property
